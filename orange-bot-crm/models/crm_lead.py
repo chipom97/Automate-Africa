87940c0a10b0a85e474982552c5bd9d68c5c1c23
Chipo Mazhani <chipomazhani@gmail.com>
2025-05-27 10:00:19 +0200

    orange bot files

---
+++ orange-bot-crm/models/crm_lead.py
@@ -0,0 +1,15 @@
+from odoo import models, fields
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+    """
+    incident_date = fields.Char(string="Date")
+    relationship = fields.Char(string="Relationship")
+    id_number = fields.Char(string="ID Number")
+    description = fields.Text(string="Description of Incident")
+    """
+    network_issue = fields.Selection([
+        ('No signal', 'No signal'),
+        ('Call drops', 'Call drops'),
+            ('Poor call quality','Poor call quality')
+    ], string='Network Issue')
